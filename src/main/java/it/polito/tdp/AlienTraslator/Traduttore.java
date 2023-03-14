a73d712fd5d42eaab61aaa8e10e1a434957575d1
ronal <ronal@DESKTOP-H2RJPFN>
2023-03-14 15:24:16 +0100

    Progetto finito

---
+++ src/main/java/it/polito/tdp/AlienTraslator/Traduttore.java
@@ -0,0 +1,36 @@
+package it.polito.tdp.AlienTraslator;
+
+import java.util.*;
+
+public class Traduttore {
+	
+	TreeMap<String, HashSet<String>> dizionario;
+	
+	public Traduttore() {
+		dizionario = new TreeMap<String, HashSet<String>>();
+	}
+	
+	public void aggiungi(String s, String s1) {
+	    HashSet<String> significati = dizionario.get(s);
+	    if (significati == null) {
+	        significati = new HashSet<String>();
+	        dizionario.put(s, significati);
+	    }
+	    significati.add(s1);
+	}
+
+	public String traduci (String s) {
+		String significati = "";
+		if (dizionario.containsKey(s)){
+			for (String p : dizionario.get(s)) {
+				significati += p + "\n";
+			}
+			}
+		else {
+			significati = "La parola non esiste nel dizionario";
+		}
+		
+		return significati;
+		
+	}
+}
--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,23 +1,82 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.AlienTraslator.Traduttore;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	private Traduttore traduttore = new Traduttore ();
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Button btClear;
+
+    @FXML
+    private Button btnTranslate;
+
+    @FXML
+    private TextField txtInput;
+
+    @FXML
+    private TextArea txtOutput;
+    
+    public void inserisci(String s) {
+    	String[] riga = s.split(" ");
+    	
+    	riga[0] = riga[0].toLowerCase();
+    	
+    	int lunghezza = riga.length;
+    	if (lunghezza == 2) {
+    		this.traduttore.aggiungi(riga[0], riga[1]);
+    		txtOutput.setText("Aggiunta parola: " + riga[0] +" al dizionario");
+    	}
+    	else if (riga.length == 1) {
+    		String traduzione = traduttore.traduci(riga[0]);
+    		txtOutput.setText(traduzione);
+    	}
+    	
+    	
+    }
+    
+
+    @FXML
+    void doClear(ActionEvent event) {
+    	txtInput.setText("");
+    	txtOutput.setText("");
+    }
+
+    @FXML
+    void doTraslate(ActionEvent event) {
+    	String parola = txtInput.getText();
+    	if (parola.matches("[a-zA-Z\\s]+")) {
+    		inserisci(parola);
+    	}
+    	else {txtOutput.setText("Inserire lettere alfabetiche");}
+    	
+    }
+    
+    public void setTraduttore(Traduttore t) {
+    	this.traduttore = t;
+    }
+
+    @FXML
     void initialize() {
+        assert btClear != null : "fx:id=\"btClear\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTranslate != null : "fx:id=\"btnTranslate\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
