31429f1982756cebaab0fa9a7940b7aa9653c407
ronal <ronal@DESKTOP-H2RJPFN>
2023-03-14 15:29:01 +0100

    ottimizzata classe "traduttore"

--- src/main/java/it/polito/tdp/AlienTraslator/Traduttore.java
+++ src/main/java/it/polito/tdp/AlienTraslator/Traduttore.java
@@ -3,34 +3,26 @@ package it.polito.tdp.AlienTraslator;
 import java.util.*;
 
 public class Traduttore {
-	
-	TreeMap<String, HashSet<String>> dizionario;
-	
-	public Traduttore() {
-		dizionario = new TreeMap<String, HashSet<String>>();
-	}
-	
-	public void aggiungi(String s, String s1) {
-	    HashSet<String> significati = dizionario.get(s);
-	    if (significati == null) {
-	        significati = new HashSet<String>();
-	        dizionario.put(s, significati);
-	    }
-	    significati.add(s1);
-	}
+    private final TreeMap<String, HashSet<String>> dizionario;
 
-	public String traduci (String s) {
-		String significati = "";
-		if (dizionario.containsKey(s)){
-			for (String p : dizionario.get(s)) {
-				significati += p + "\n";
-			}
-			}
-		else {
-			significati = "La parola non esiste nel dizionario";
-		}
-		
-		return significati;
-		
-	}
+    public Traduttore() {
+        dizionario = new TreeMap<>();
+    }
+
+    public void aggiungi(String s, String s1) {
+        dizionario.computeIfAbsent(s, k -> new HashSet<>()).add(s1);
+    }
+
+    public String traduci(String s) {
+        StringBuilder significati = new StringBuilder();
+        HashSet<String> set = dizionario.get(s);
+        if (set != null) {
+            for (String p : set) {
+                significati.append(p).append("\n");
+            }
+        } else {
+            significati.append("La parola non esiste nel dizionario");
+        }
+        return significati.toString();
+    }
 }
